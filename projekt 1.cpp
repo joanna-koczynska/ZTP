fb6944352dbdec6433a3ea242867fbccaee4f473
Joanna Koczyńska <110031257+joanna-koczynska@users.noreply.github.com>
2025-03-19 21:04:06 +0100

    Add files via upload

---
+++ projekt 1.cpp
@@ -0,0 +1,17 @@
+﻿// projekt 1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+	while (true)
+	{
+		cin >> x;
+		if (x == 42)
+			break;
+		cout <<x<<endl;
+	}
+}
